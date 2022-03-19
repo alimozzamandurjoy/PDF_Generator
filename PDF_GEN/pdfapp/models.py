0c4b6bb1a1b7686260a9e8cc99189e6d1e0a27a0
alimozzamandurjoy <alimozzamandurjoy@gmail.com>
2022-03-19 21:27:07 +0600

    app created

---
+++ PDF_GEN/pdfapp/models.py
@@ -0,0 +1,15 @@
+from distutils.archive_util import make_zipfile
+from django.db import models
+
+# Create your models here.
+
+
+class Profile(models.Model):
+    name= models.CharField(max_length=200)
+    email= models.EmailField(max_length=200)
+    phone= models.CharField(max_length=100)
+    summary= models.TextField(max_length=2000)
+    degree= models.CharField(max_length=200)
+    university= models.CharField(max_length=200)
+    previous_work= models.CharField(max_length=1000)
+    skills= models.TextField(max_length=1000)
